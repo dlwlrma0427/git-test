c83dbc1cec83792172f3fe7e632f162c5baf342d
jxy <jiangxiaoyangu@163.com>
2022-02-27 14:53:22 +0800

    hot-fix commit2

--- src/main/java/com/a/GitTest.java
+++ src/main/java/com/a/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("hello fix");
+        System.out.println("hello fix-test");
     }
 }
